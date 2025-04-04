87d0d8300cdaa106370e8574d0e837c2fc056a0c
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-04 11:00:50 -0300

    Update Main.java

--- src/com/Leonardo/taskboard/view/Main.java
+++ src/com/Leonardo/taskboard/view/Main.java
@@ -1,1 +1,38 @@
+package com.seu_nome.taskboard.view;
+
+import com.seu_nome.taskboard.service.BoardService;
+import com.seu_nome.taskboard.db.ConexaoDB;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static BoardService boardService;
+
+    public static void main(String[] args) {
+        boardService = new BoardService(ConexaoDB.getConnection());
+
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Criar novo board");
+            System.out.println("2. Selecionar board");
+            System.out.println("3. Excluir boards");
+            System.out.println("4. Sair");
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome do novo board:");
+                    String nomeBoard = scanner.next();
+                    boardService.criarBoard(nomeBoard);
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    return;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
+    }
+}
 
