97a926cad7303c9131881e2346da6de18ca5a288
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-04-04 11:00:14 -0300

    Update Board.java

--- src/com/Leonardo/taskboard/model/Board.java
+++ src/com/Leonardo/taskboard/model/Board.java
@@ -1,1 +1,12 @@
+package com.seu_nome.taskboard.model;
+
+import java.util.List;
+
+public class Board {
+    private int id;
+    private String nome;
+    private List<Coluna> colunas;
+
+    // Getters e setters
+}
 
